cd06c91833f2000bba9f6730661837e860d3c99d
Lucius-lsr <462114286@qq.com>
2022-12-19 22:31:13 +0800

    first commit

---
+++ raw/process.py
@@ -0,0 +1,57 @@
+import json
+
+def colorMap(area):
+    fill, stroke = None, '#000000'
+    if area == "以绪塔尔":
+        fill, stroke = '#FF00FF', '#FFFF00'  # Red # Yellow
+    elif area == "巨神峰":
+        fill, stroke = '#FFA500', '#800080'
+    elif area == "弗雷尔卓德":
+        fill, stroke = '#FFFF00', '#008000'
+    elif area == "德玛西亚":
+        fill, stroke = '#008000', '#0000FF'
+    elif area == "恕瑞玛":
+        fill, stroke = '#FF0000', '#FFC0CB'
+    elif area == "暗影岛":
+        fill, stroke = '#A040A0', '#FF0000'
+    elif area == "比尔吉沃特":
+        fill, stroke = '#FFC0CB', '#FFA500'
+    elif area == "班德尔城":
+        fill, stroke = '#FF00FF', '#FFFF00'
+    elif area == "皮尔特沃夫":
+        fill, stroke = '#008080', '#008000'
+    elif area == "祖安":
+        fill, stroke = '#40E0D0', '#0000FF'
+    elif area == "艾欧尼亚":
+        fill, stroke = '#C6C6FA', '#800080'
+    elif area == "虚空之地":
+        fill, stroke = '#FF7F50', '#FF0000'
+    elif area == "诺克萨斯":
+        fill, stroke = '#ADD8E6', '#006400'
+    return {'fill': fill, 'stroke':'#000000'}
+
+# Open the JSON file
+with open('hero_meta.json', 'r') as f:
+  # Load the contents of the file into a variable
+  data = json.load(f)
+
+res = {'nodes':[], 'edges':[]}
+for hero in data:
+    res['nodes'].append({
+    'id':hero['zh_name'],
+     'label':hero['zh_name'],
+      'details':hero,
+      'style': colorMap(hero['相关地区']),
+      'labelCfg': {
+          'style': {
+            'fontSize': 8
+          }
+        }
+      })
+for hero in data:
+    for rela in hero['relative_heros']:
+        res['edges'].append({'source':hero['zh_name'], 'target':rela['zh_name']})
+
+with open('../public/relations.json', 'w') as outfile:
+  # Write the dictionary to the file in JSON format
+  json.dump(res, outfile)
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import G6 from '@antv/g6';
+
+const container = document.getElementById('container');
+const width = container.scrollWidth;
+const height = container.scrollHeight || 1000;
+const graph = new G6.Graph({
+    container: 'container',
+    width,
+    height,
+    layout: {
+        type: 'force',
+        preventOverlap: true,
+    },
+    defaultNode: {
+        size: 20,
+    },
+    modes: {
+        default: ['drag-canvas', 'zoom-canvas', 'drag-node', // 允许拖拽画布、放缩画布、拖拽节点
+            {
+                type: 'tooltip', // 提示框
+                formatText(model) {
+                    // 提示框文本内容
+                    return '角色定位: ' + model.details["角色定位"] + '<br/>' +
+                        '相关地区: ' + model.details["相关地区"] + '<br/>' +
+                        '英文名: ' + model.details["en_name"] + '<br/>' +
+                        '中文名: ' + model.details["zh_name"]
+                },
+            },
+        ],
+    },
+});
+
+fetch('relations.json')
+    .then((res) => res.json())
+    .then((data) => {
+        graph.data({
+            nodes: data.nodes,
+            edges: data.edges.map(function (edge, i) {
+                edge.id = 'edge' + i;
+                return Object.assign({}, edge);
+            }),
+        });
+        graph.render();
+
+        graph.on('node:dragstart', function (e) {
+            graph.layout();
+            refreshDragedNodePosition(e);
+        });
+        graph.on('node:drag', function (e) {
+            const forceLayout = graph.get('layoutController').layoutMethods[0];
+            forceLayout.execute();
+            refreshDragedNodePosition(e);
+        });
+        graph.on('node:dragend', function (e) {
+            e.item.get('model').fx = null;
+            e.item.get('model').fy = null;
+        });
+
+        if (typeof window !== 'undefined')
+            window.onresize = () => {
+                if (!graph || graph.get('destroyed')) return;
+                if (!container || !container.scrollWidth || !container.scrollHeight) return;
+                graph.changeSize(container.scrollWidth, container.scrollHeight);
+            };
+    });
+
+function refreshDragedNodePosition(e) {
+    const model = e.item.get('model');
+    model.fx = e.x;
+    model.fy = e.y;
+}
